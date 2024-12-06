8db3dff27b19025124e2cc73d13f0551f1242b91
Elyrial <alexpersson03@proton.me>
2024-12-06 13:58:34 +0100

    Small update

--- day06/a.cpp
+++ day06/a.cpp
@@ -1,21 +1,34 @@
 #include <bits/stdc++.h>
 
+using namespace std;
+
+bool isOutOfBounds(int x, int y, int rows, int cols) {
+  return x < 0 || x >= rows || y < 0 || y >= cols;
+}
+
 int main() {
   // Input map
-  std::vector<std::string> map;
-  std::string row;
+  vector<string> map;
+  string row;
   while (std::cin >> row) {
     map.push_back(row);
   }
 
+  int rows = map.size();
+  int cols = map[0].size();
+
+  vector<pair<int, int>> directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+  char directionSymbols[] = {'^', '>', 'v', '<'};
+  int initialDirectionIndex;
+
   // Find player position and initial direction
   int x, y;
-  for (int i = 0; i < map.size(); i++) {
-    for (int j = 0; j < map[j].length(); j++) {
-      if (map[i][j] == '^') {
-        x = j;
-        y = map.size() - i - 1;
-        break;
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      if (map[i][j] == '^' || map[i][j] == '>' || map[i][j] == 'v' ||
+          map[i][j] == '<') {
+        x = i;
+        y = j;
       }
     }
   }
